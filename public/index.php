b42c881d4255196ec78da26cefb97c6341f9d2f8
SeniruR <seniruranasinghe@gmail.com>
2024-11-15 21:12:45 +0530

    Changes added.

--- public/index.php
+++ public/index.php
@@ -34,7 +34,6 @@ $alcontroller = new AdminLoginController($database);
 // Get the URL parameter
 $url = isset($_GET['url']) ? $_GET['url'] : '';
 
-// Route the request to the appropriate controller method
 switch ($url) {
     case 'event.php':
         $hcontroller->render();
