171bef2287a7ac13b3a6cd28657e33174ebe4b0e
navindu <navinduthilakshana9@gmail.com>
2024-11-29 17:11:47 +0530

    staff.php and css

---
+++ app/views/events/staff.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="staffstyles.css"> 
+</head>
+<body>
+    <center>
+    <h2>Event Requests</h2>
+    </center>
+    <div class="tb">
+        <div class="table-header">
+            <table>
+                <thead>
+                <tr>
+                    <th>Event</th>
+                    <th>Event Organizer</th>
+                    <th>Location</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Accept</th>
+                    <th>Reason for rejection</th>
+                    <th>Reject</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Vani Vizha</td>
+                        <td>Vanitha</td>
+                        <td>UCSC Auditorium</td>
+                        <td>2024-09-30</td>
+                        <td>10.00 AM</td>
+                        <td><button type="button" class="accept">Accept</button></td>
+                        <td><textarea placeholder="Reason for rejection" rows="3"></textarea></td>
+                        <td>
+                                <button type="button" class="reject">Reject</button>
+                        </td>
+                        
+                        
+                    </tr> 
+                    <tr>
+                        <td>Reid handawa</td>
+                        <td>Sandun</td>
+                        <td>UCSC Auditorium</td>
+                        <td>2024-09-03</td>
+                        <td>08.00 PM</td>
+                        <td><button type="button" class="accept">Accept</button></td>
+                        <td><textarea placeholder="Reason for rejection" rows="3"></textarea></td>
+                        <td>
+                                <button type="button" class="reject">Reject</button>
+                        </td>
+                        
+                        
+                    </tr>
+                    <tr>
+                        <td>Padura</td>
+                        <td>Naveen</td>
+                        <td>UCSC Auditorium</td>
+                        <td>2024-05-07</td>
+                        <td>07.00 PM</td>
+                        <td><button type="button" class="accept">Accept</button></td>
+                        <td><textarea placeholder="Reason for rejection" rows="3"></textarea></td>
+                        <td>
+                                <button type="button" class="reject">Reject</button>
+                        </td>
+                        
+                        
+                    </tr>
+                </tbody>
+            </table>
+        </div>  
+    </div>
+    
+</body>
+</html>
